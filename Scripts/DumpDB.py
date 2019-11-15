f074a10771e1c9671e50532b9ed959df5176a0c6
Lisa Malins <lkmalins@ucdavis.edu>
2019-11-15 14:18:13 -0800

    In DumpDB.py, added support for multiple files as arguments and multiple tables per file

--- Scripts/DumpDB.py
+++ Scripts/DumpDB.py
@@ -13,23 +13,28 @@ import sqlite3
 
 # Verify argument number
 usage = "Usage: python DumpDB.py input.db\n"
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
     sys.stderr.write(usage)
     exit(1)
 
+
 # Connect to database
-connection = sqlite3.connect(sys.argv[1])
-cursor = connection.cursor()
+for file in sys.argv[1:]:
+    print("Dumping database from file {}".format(file))
+    connection = sqlite3.connect(file)
+    cursor = connection.cursor()
 
-# Get list of tables
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-(tables, ) = cursor.fetchall()
+    # Get list of tables
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
 
-# Dump contents of all tables
-for table in tables:
-    print("Table name: {}".format(table))
-    cursor.execute("SELECT * FROM {}".format(table))
-    row = cursor.fetchone()
-    while row:
-        print(row)
+    # Dump contents of all tables
+    for (table, ) in tables:
+        print("\nTable name: {}".format(table))
+        cursor.execute("SELECT * FROM {}".format(table))
         row = cursor.fetchone()
+        while row:
+            print(row)
+            row = cursor.fetchone()
+
+    print("\n")
